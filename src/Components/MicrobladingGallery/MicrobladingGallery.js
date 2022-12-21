1ef7569e916591b7090d685592e80c2f2e33a9d8
Laurynas Juzva <laurynas.juzva@gmail.com>
2022-12-20 21:48:46 -0800

    order for photos

--- src/Components/MicrobladingGallery/MicrobladingGallery.js
+++ src/Components/MicrobladingGallery/MicrobladingGallery.js
@@ -2,13 +2,23 @@ import { useState } from "react";
 import Slider from 'react-slick'
 import {FaArrowRight, FaArrowLeft} from 'react-icons/fa'
 import './MicrobladingGallery.css'
+import California from '../../Assets/Gallery/7.png'
+import Lady from '../../Assets/Gallery/10.png' 
+import Woman from '../../Assets/Gallery/9.png'
+import Coronita from '../../Assets/Gallery/3.png'
+import Dos_Lagos from '../../Assets/Gallery/2.png'
+import Semi from '../../Assets/Gallery/Semi.png' 
+import Microblading from '../../Assets/Gallery/Microblading.png'
+import Socal from '../../Assets/Gallery/6.png'
+import Natural from '../../Assets/Gallery/12.png'
+import Powder_Brow from '../../Assets/Gallery/Powder_Brow.png'
+import Temescal_Valley from '../../Assets/Gallery/1.png'
+import Woodcrest from '../../Assets/Gallery/4.png'
 import lipBlush from '../../Assets/Gallery/lip_blush.png'
 import pmu from '../../Assets/Gallery/pmu.png'
 import Ombré from '../../Assets/Gallery/Ombré.png'
-import Powder_Brow from '../../Assets/Gallery/Powder_Brow.png'
 import Touch_Up from '../../Assets/Gallery/Touch_Up.png'
 import Combination_Brow from '../../Assets/Gallery/Combination_Brow.png'
-import Microblading from '../../Assets/Gallery/Microblading.png'
 import BROW_DADDY from '../../Assets/Gallery/BROW_DADDY.png'
 import pmuangel from '../../Assets/Gallery/pmuangel.png'
 import Corona from '../../Assets/Gallery/Corona.png'
@@ -16,29 +26,15 @@ import Riverside from '../../Assets/Gallery/Riverside.png'
 import brows from '../../Assets/Gallery/brows.png'
 import eyebrow from '../../Assets/Gallery/eyebrow.png'
 import Permanent from '../../Assets/Gallery/Permanent.png'
-import Semi from '../../Assets/Gallery/Semi.png' 
 import tattoo from '../../Assets/Gallery/tattoo.png'
 import technique from '../../Assets/Gallery/technique.png'
-
-
-import Temescal_Valley from '../../Assets/Gallery/1.png'
-import Dos_Lagos from '../../Assets/Gallery/2.png'
-import Coronita from '../../Assets/Gallery/3.png'
-import Woodcrest from '../../Assets/Gallery/4.png'
 import Perris from '../../Assets/Gallery/5.png'
-import Socal from '../../Assets/Gallery/6.png'
-import California from '../../Assets/Gallery/7.png'
 import Girl from '../../Assets/Gallery/8.png'
-import Woman from '../../Assets/Gallery/9.png'
-import Lady from '../../Assets/Gallery/10.png' 
 import Best from '../../Assets/Gallery/11.png'
-import Natural from '../../Assets/Gallery/12.png'
 
 
-const images = [ lipBlush, pmu, Ombré, Powder_Brow, Touch_Up, Combination_Brow, 
-  Microblading, BROW_DADDY, pmuangel, Corona, Riverside, brows, eyebrow, Permanent, 
-  Semi, tattoo, technique, Temescal_Valley, Dos_Lagos, Coronita, Woodcrest, Perris, 
-  Socal, California, Girl, Woman, Lady, Best, Natural ]
+
+const images = [ California, Lady, Woman, Coronita, Dos_Lagos, Semi, Microblading, Socal, Natural, Powder_Brow, Temescal_Valley, Woodcrest, lipBlush, pmu, Ombré, Touch_Up, Combination_Brow, BROW_DADDY, pmuangel, Corona, Riverside, brows, eyebrow, Permanent, tattoo, technique, Perris, Girl, Best ]
 
 function MicrobladingGallery() {
   const NextArrow = ({ onClick }) => {
