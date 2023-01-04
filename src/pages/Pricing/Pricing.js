4815a38aa4716646700c019112d63a805ff3fc45
Laurynas Juzva <laurynas.juzva@gmail.com>
2023-01-03 23:03:10 -0800

    booking update

--- src/pages/Pricing/Pricing.js
+++ src/pages/Pricing/Pricing.js
@@ -7,7 +7,7 @@ function Pricing() {
   return (
     <div className="Pricing">
        <ServicesComponent />
-       <Row className="Pricing__row" >
+       {/* <Row className="Pricing__row" >
        <h1 className="Pricing__h1">
        PMU INITIAL SERVICE PRICE
          </h1>
@@ -87,7 +87,93 @@ function Pricing() {
               </a>
          </Col>
 
+       </Row> */}
+
+       {/* TEST */}
+          <Row className="Pricing__row" >
+       <h1 className="Pricing__h1">
+       PMU INITIAL SERVICE PRICE
+         </h1>
        </Row>
+       <Row className="Pricing__row" >
+       <h2 id="Pricing__anchor__tag"  className="Pricing__h2__mob">
+       AN IN-DEPTH CONSULTATION IS PROVIDED PRIOR TO BEGINNING THE PROCEDURE.
+         </h2>
+       </Row>
+       <Row className="Pricing__row__3" >
+
+       <Col id="Pricing__R1__col1" xl="3">
+       <h2 className="Pricing__h2">
+        / Lip Blush /
+         </h2>
+         <p className="Pricing__p"><b>
+         Lip Blush tattoo enhances the natural shape of your lips, giving them that boost of vitality and even coloring. Lip blush tattoos are designed to last 2 to 3 years.
+         </b>
+         </p>
+          <h2 className="Pricing__h2__price">
+        / $450 /
+         </h2>
+         <a href="facetime:17472247451‬" >     
+          <button id='Pricing__row1__button'>
+              Call To Book</button>
+              </a>
+         </Col>
+         <Col id="Pricing__R1__col1" xl="3">
+         <h2 className="Pricing__h2">
+        / Powder Brows /
+         </h2>
+         <p className="Pricing__p"><b>
+         Powder brows can be done as light as a brow tint or as dark as glam. Recommended for those who want a completely "filled in" brow! Best for ALL skin types.
+         </b>
+         </p>
+          <h2 className="Pricing__h2__price">
+        / $450 /
+         </h2>
+         <a href="facetime:17472247451‬" >     
+          <button id='Pricing__row1__button'>
+          Call To Book</button>
+              </a>
+         </Col>
+         <Col id="Pricing__R1__col1" xl="3">
+         <h2 className="Pricing__h2">
+        / Microblading /
+         </h2>
+         <p className="Pricing__p"><b>
+         Microblading is a permanent procedure that mimics the same size of an eyebrow hair stroke through small incisions made with a microblade.
+
+         </b>
+         </p>
+          <h2 className="Pricing__h2__price">
+        / $450 /
+         </h2>
+         <a href="facetime:17472247451‬" >     
+          <button id='Pricing__row1__button'>
+          Call To Book</button>
+              </a>
+         </Col>
+
+         <Col id="Pricing__R1__col1" xl="3">
+         <h2 className="Pricing__h2">
+        / Combo Brows /
+         </h2>
+         <p className="Pricing__p"><b>
+         This technique offers the best of both powdered and hair stroke effects combined together to achieve a fluffy, textured, yet defined brow.
+
+         </b>
+         </p>
+          <h2 className="Pricing__h2__price">
+        / $450 /
+         </h2>
+         <a href="facetime:17472247451‬" >     
+          <button id='Pricing__row1__button'>
+          Call To Book</button>
+              </a>
+         </Col>
+
+       </Row>
+
+{/* TEST */}
+
        <Row className="Pricing__row" >
        <h1 className="Pricing__h1__4">
        TOUCH UPS
